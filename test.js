01f49dc2ec396ae7a4509c4dcd6760e65f9f6488
Jamund Ferguson <jamund@gmail.com>
2014-01-27 10:22:38 -0800

    more tests

--- test.js
+++ test.js
@@ -8,5 +8,15 @@ config.set("features", {
 	featureB: false
 });
 
+// test the basic stuff
 assert.ok(neff.isEnabled("featureA"), "feature a should be enabled");
 assert.ok(!neff.isEnabled("featureB"), "feature b should not be enbled");
+
+// test the helpers
+var req = {};
+var res = {locals:{}};
+neff.helpers(req, res, function() {
+	assert.equal(res.locals.featureClasses, "feature-featureA", "we should have a correct CSS string");
+	assert.ok(res.locals['feature-featureA'], "feature a should exist in locals");
+	assert.ok(!res.locals['feature-featureB'], "feature b should not exist in locals");
+});

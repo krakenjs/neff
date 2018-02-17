c09c981c637ceeb96e39f2878abc8fa159bbbb45
Jamund Ferguson <jamund@gmail.com>
2018-02-16 20:27:19 -0800

    API Update
    Added support for `addFeatures()` to dynamically add features and moved the feature class work into an `init()` function.

--- cjs.js
+++ cjs.js
@@ -4,19 +4,37 @@
 "use strict";
 
 // grab the features off of the <body>
-var prefix = "feature-";
-var classes = document.body.className.split(/\s/);
 var features = {};
-for (var i = 0; i < classes.length; i++) {
-	if (classes[i].indexOf(prefix) === 0) {
-		features[classes[i].replace(prefix, "")] = true;
+
+function getFeaturesFromClassNames() {
+	var prefix = "feature-";
+	var classes = document.body.className.split(/\s/);
+	var f = []
+	for (var i = 0; i < classes.length; i++) {
+		if (classes[i].indexOf(prefix) === 0) {
+			f.push(classes[i].replace(prefix, ""))
+		}
+	}
+	return f
+}
+
+// a simple API for adding an array of features
+function addFeatures(f) {
+	for (var i = 0; i < features.length; i++) {
+		features[f[i]] = true
 	}
 }
 
-// return a simple API that lets us determine if features are available
+// a simple API that lets us determine if features are available
 // Ex: if (feature.isEnabled("your-feature")) { /* ... */ }
-module.exports = {
-	isEnabled: function(feature) {
-		return features[feature];
-	}
-};
+function isEnabled(feature) {
+	return features[feature] || false;
+}
+
+function init() {
+	var classFeatures = getFeaturesFromClassNames()
+	addFeatures(classFeatures)
+}
+
+module.exports = { init: init, isEnabled: isEnabled, addFeatures: addFeatures };
+
